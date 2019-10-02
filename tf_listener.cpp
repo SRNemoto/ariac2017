f79f2798dbee4b0df725c83fc297a9df70d02885
Shota Nemoto <srn24@case.edu>
2019-10-02 18:18:20 -0400

    Finally got the code to compile

--- tf_listener.cpp
+++ tf_listener.cpp
@@ -1,4 +1,4 @@
-#include "mvoeit/move_group_interface/move_group_interface.h"
+#include "moveit/move_group_interface/move_group_interface.h"
 #include "moveit/planning_scene_interface/planning_scene_interface.h"
 #include "tf2_ros/transform_listener.h"
 #include "tf2_geometry_msgs/tf2_geometry_msgs.h"
@@ -7,62 +7,67 @@
 #include "osrf_gear/Order.h"
 #include "osrf_gear/Model.h"
 #include "ros/ros.h"
+#include "std_msgs/String.h"
 #include "std_srvs/Trigger.h"
-#include <buffer.h>
+#include "tf2_ros/buffer.h"
 #include <vector>
 
-std::vector<std_msgs::init> order_vec;
+std::vector<osrf_gear::Order> order_vec;
 
 osrf_gear::LogicalCameraImage camera_msg;
 
-void receiveOrder(osrf_gear::Order &order) {
-    order_vec.push_back(order)
+void receiveOrder(const osrf_gear::Order &order) {
+    order_vec.push_back(order);
 }
 
-void seeObjects(osrf_gear::LogicalCameraImage &msg) {
+void seeObjects(const osrf_gear::LogicalCameraImage &msg) {
     camera_msg = msg;
 }
 
 int main(int argc, char** argv) {
-    int_vector.clear();
+    order_vec.clear();
 
     //Initialize ROS node
-    ros::init(argc, argvv, "tf_listener");
+    ros::init(argc, argv, "tf_listener_node");
     ros::NodeHandle n;
 
     // Trigger the start of the competition
+
     ros::ServiceClient begin_client = n.serviceClient<std_srvs::Trigger>("Service Name");
     std_srvs::Trigger begin_comp;
     begin_client.call(begin_comp);
 
+
     // Warning message?
-    ROS_WARN("Competition service returned failure: %s", begin_comp.response.message.c_str());
+    //ROS_WARN("Competition service returned failure: %s", begin_comp.response.message.c_str());
 
     // Subscriber to receive orders
-    ros::Subscriber sub = n.subscribe("/ariac/orders", 1000, receiveOrder); 
+    ros::Subscriber order_sub = n.subscribe("/ariac/orders", 1000, receiveOrder); 
 
     // Subscriber to receive camera location info
-    ros::Subscriber sub = n.subscribe("/ariac/logical_camera", 1000, seeObjects); 
+    ros::Subscriber cam_sub = n.subscribe("/ariac/logical_camera", 1000, seeObjects); 
+
 
     // Transformation buffer
     tf2_ros::Buffer tfBuffer;
     tf2_ros::TransformListener tfListener(tfBuffer);
 
+
     // Instance of a move group for MoveIt to create motion plans
     moveit::planning_interface::MoveGroupInterface move_group("manipulator");
 
-    geometry_msgs:TransformStamped tfStamped;
+    // Retrieve the transformation
+    geometry_msgs::TransformStamped tfStamped;
     try {
         tfStamped = tfBuffer.lookupTransform(
-            move_group.getPlanningFrame().cstr(),
-            "logical_camera_frame",
+            move_group.getPlanningFrame().c_str(),
+            "/ariac/logical_camera",
             ros::Time(0.0),
             ros::Duration(1.0));
         ROS_DEBUG("Transform to [%s] from [%s]", tfStamped.header.frame_id.c_str(), tfStamped.child_frame_id.c_str());
     } catch (tf2::TransformException &ex) {
         ROS_ERROR("%s", ex.what());
     }
-
     // tf2_ross::Buffer.lookupTransform("to_frame", "from_frame", "how_recent", "how_long_to_wait");
 
     // Create Variables
@@ -71,11 +76,10 @@ int main(int argc, char** argv) {
     // Copy pose from the logical camera.
     //part_pose.pose = ;
 
-    tf2::doTransform(part_pose, goal_pose, transformStamped);
-
+    tf2::doTransform(part_pose, goal_pose, tfStamped);
 
     // See the first piston part from the camera
-    Model piston_part = camera_msg.models[0];
+    osrf_gear::Model piston_part = camera_msg.models[0];
 
     // Add height to goal pose.
     goal_pose.pose.position.z += 0.10; // 10 cm above the part
@@ -86,6 +90,7 @@ int main(int argc, char** argv) {
     goal_pose.pose.orientation.y = 0.707;
     goal_pose.pose.orientation.z = 0.0;
 
+
     // Set the desired pose for the arm in the arm controller
     move_group.setPoseTarget(goal_pose);
 
@@ -94,7 +99,6 @@ int main(int argc, char** argv) {
 
     // Create a plan based on teh settings (all default settings now) in the_plan.
     
-
      // TODO: Check output of the plan 
     if (move_group.plan(the_plan)) {
         // In the event the plan was created, execute.
@@ -104,9 +108,10 @@ int main(int argc, char** argv) {
 
 
     while(ros::ok()) {
-        if (int_vector.size() > 0) {
+        if (order_vec.size() > 0) {
             // TODO 
         }
     }
+
 }
 
